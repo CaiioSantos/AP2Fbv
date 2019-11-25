10f4c5a5ef247265febe418790e89be47459201c
CaiioSantos <caiohgs13@gmail.com>
2019-11-25 01:26:27 -0300

    Criando parte logica do funcionanmento da tela de adicionar contatos

---
+++ app/src/main/java/com/example/downloadmanager/UI/ListaDeContatos.java
@@ -0,0 +1,14 @@
+package com.example.downloadmanager.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaDeContatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_de_contatos);
+    }
+}
